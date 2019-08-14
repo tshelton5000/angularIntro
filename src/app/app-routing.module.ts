c78e06a17a91f28eafe0ee8552d1e5df22ee7aec
admin <ts70@evansville.edu>
2019-08-13 20:05:32 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuoteDisplayComponent } from './quote-display/quote-display.component';
+import { CustomViewComponent } from './custom-view/custom-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: QuoteDisplayComponent},
+  {path: 'customview', component: CustomViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
